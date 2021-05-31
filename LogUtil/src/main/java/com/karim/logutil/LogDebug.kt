0d65308a3ee33ce0eeb21067cf5128eb1e0e8f4c
karimelbahi <karimmaster333@gmail>
2021-05-31 13:38:18 +0200

    3th Edit for DebugUtil and ErrorUtil moduls

--- LogUtil/src/main/java/com/karim/logutil/LogDebug.kt
+++ LogUtil/src/main/java/com/karim/logutil/LogDebug.kt
@@ -5,7 +5,7 @@ import android.util.Log
 object LogDebug {
 
     private val TAG: String = "SUPER_AWESOME_APP_LIB"
-    public fun debug(message: String) {
+    fun debug(message: String) {
         Log.d(TAG, message)
     }
 }
--- app/src/main/java/com/karim/libexample/MainActivity.kt
+++ app/src/main/java/com/karim/libexample/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        ErrorDebug.errorr("Error test")
+        ErrorDebug.error("Error test")
         LogDebug.debug("Debug test")
     }
 
